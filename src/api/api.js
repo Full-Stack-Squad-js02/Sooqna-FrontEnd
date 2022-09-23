cddedffebdd5e02a8b70547baea4d546a33521ef
EsraaBanat <esraa.bnt96@gmail.com>
2022-09-23 20:12:24 +0300

    search functioality

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,15 @@
 
-export const searchBy = (input,filterdBy) => {
-    let url = `https://sooqna.herokuapp.com/search${filterdBy}?${filterdBy}=${input}`
+// export const getAllProducts = (input,filterdBy) => {
+//     let url = `https://sooqna.herokuapp.com/search${filterdBy}?${filterdBy}=${input}`
+//         return fetch(url, {method: "GET"}).then(response => {
+//         return response.json();
+//     }).catch(err => console.log(err));
+// }
+export const searchBy = ({input,filteredBy}) => {
+    console.log(input,filteredBy)
+    let url = `https://sooqna.herokuapp.com/search${filteredBy}?${filteredBy}=${input}`
         return fetch(url, {method: "GET"}).then(response => {
         return response.json();
     }).catch(err => console.log(err));
 }
+
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,83 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Tabs, Tab, Box} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 import Button from '@mui/material/Button';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import { Form, Dropdown, DropdownButton } from 'react-bootstrap';
-import {logOut} from '../../auth';
+import {Form, Dropdown, DropdownButton} from 'react-bootstrap';
+import { logOut } from '../../auth';
+import {searchBy} from '../../api/api'
+import './Navbar.css';
 // import PageviewOutlinedIcon from '@mui/icons-material/PageviewOutlined';
 
-
 export default function NavBar() {
-    const [search, seSearch] = useState({
-        input: '',
-        filteredBy:''
-    });
+    const [search,
+        seSearch] = useState({input: '', filteredBy: ''});
 
     const navigate = useNavigate();
     const [value,
         setValue] = React.useState('one');
-    
-    const filterBy = (event) =>{
-        // console.log(event);
-        //     seSearch({
-        //     ...search,
-        //         filteredBy: event,
-        //   });
-    }
 
-
-    // console.log(value)
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-    
+
     function handleLogOut() {
         logOut();
         navigate('/')
     }
-    
-    // function handleNavigate() {
-    //     if(value === 'one')  navigate('/')
-    // }
+console.log('jjj', search)
+    // function handleNavigate() {     if(value === 'one')  navigate('/') }
+    // useEffect(() => {     console.log('hhhhhhhhh',search); }, [search])
 
-    // useEffect(() => {
-    //     handleNavigate();
-    // }, [])
-    
     return (
-        <Box sx={{
+        <Box
+            sx={{
             width: '100%'
+        }}
+            style={{
+            display: 'flex'
         }}>
             <Tabs
                 value={value}
                 onChange={handleChange}
                 textColor="secondary"
                 indicatorColor="secondary"
-                aria-label="secondary tabs example">
-                <Tab value="one" label="Home" />
+                aria-label="secondary tabs example"
+                style={{
+                flex: 'auto'
+            }}>
+                <Tab value="one" label="Home"/>
                 <Tab value="two" label="About"/>
                 <Tab value="three" label="Contact Us"/>
             </Tabs>
-                <Button variant="contained" onClick={() => navigate('/signup')}>Sign Up</Button>
-                <Button variant="outlined" onClick={() => navigate('/signin')}>Sign In</Button>
-                <Button variant="outlined" onClick={handleLogOut} >Log Out</Button>
-            <div style={{display:'flex'}}>
+            <div style={{
+                display: 'flex'
+            }}>
                 <DropdownButton id="dropdown-basic-button" title="Filter By">
-                    <Dropdown.Item  onClick={filterBy('name')}>Product</Dropdown.Item>
-                    <Dropdown.Item  onClick={filterBy('color')}>Color</Dropdown.Item>
-                    <Dropdown.Item  onClick={filterBy('category')} >Category</Dropdown.Item>
-                    <Dropdown.Item  onClick={filterBy('price')} >Price</Dropdown.Item>
-            </DropdownButton>
+                    <Dropdown.Item onClick={() => {
+                        seSearch({ ...search, filteredBy: 'name' })
+                        // console.log('jjj', search)
+                    }}>Product</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {seSearch({ ...search, filteredBy: 'color' })}}>Color</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {seSearch({ ...search, filteredBy: 'category' })}}>Category</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {seSearch({ ...search, filteredBy: 'price' })}} >Price</Dropdown.Item>
+                </DropdownButton>
                 <Form className="d-flex">
                     <Form.Control
                         type="search"
                         placeholder="Search"
                         className="me-2"
-                        aria-label="Search"/>
+                        aria-label="Search"
+                        onChange={(e) => { seSearch({ ...search, input: e.target.value })}}
+                    />
                 </Form>
-                <SearchOutlinedIcon fontSize="large" />
+                <SearchOutlinedIcon fontSize="large" onClick={()=>{searchBy(search)}}/>
             </div>
+            <Button variant="contained" onClick={() => navigate('/signup')}>Sign Up</Button>
+            <Button variant="outlined" onClick={() => navigate('/signin')}>Sign In</Button>
+            <Button variant="outlined" onClick={handleLogOut}>Log Out</Button>
         </Box>
-        
-    )}
+
+    )
+}
