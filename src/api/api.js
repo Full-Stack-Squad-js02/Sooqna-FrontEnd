dad774be0482847d20033e35f10eccb52a7151a1
EsraaBanat <esraa.bnt96@gmail.com>
2022-09-25 10:18:14 +0300

    api/search

--- src/api/api.js
+++ src/api/api.js
@@ -5,11 +5,19 @@
 //         return response.json();
 //     }).catch(err => console.log(err));
 // }
-export const searchBy = ({input,filteredBy}) => {
+export const searchBy = async  ({input,filteredBy}) => {
     console.log(input,filteredBy)
     let url = `https://sooqna.herokuapp.com/search${filteredBy}?${filteredBy}=${input}`
         return fetch(url, {method: "GET"}).then(response => {
         return response.json();
-    }).catch(err => console.log(err));
+        }).catch(err => console.log(err));
+    
+    //     const result = await axios.post(`https://sooqna.herokuapp.com/search${filteredBy}?${filteredBy}=${input}`, data, {
+    //     headers: {
+    //         Accept: 'application/json',
+    //         Authorization: `Bearer ${token}`
+    //     }
+    // });
+    // return result
 }
 
