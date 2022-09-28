8c4ab2ccbe129a792a7ee1662992a04778210a37
shamssar <nayefshams3@gmail.com>
2022-09-28 13:18:56 +0300

    add style to myproduct

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Signup from './components/SignUp/Signup';
 import Signin from './components/SignIn/Signin';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
-import SlideShow from './components/Home/SlideShow';
 import Product from './components/Product/Product';
+import MyProducts from './components/myProduct/myProducts';
 import ProductCard from './components/Product/productCard';
 import { PrivateRoute } from './auth/privteRoutes';
 import Contact from './components/Contact/Contact';
 import AboutPage from './components/About/About';
 import NavBar from './components/NavBar/NavBar';
-// import Bar from './components/NavBar/Bar';
+
 
 
 function App() {
@@ -22,21 +22,16 @@ function App() {
                 <NavBar/> 
                 <Routes>
                     <Route path='/' element={< Home />}/>
-                    {/* <Route path='/' element={< Ab />}/> */}
+                    <Route path='/about' element={< AboutPage />}/>
                     <Route path='/signup' element={< Signup />}/>
                     <Route path='/signin' element={<Signin/>}/>
                     <Route path='/product' element={<PrivateRoute><Product/></PrivateRoute>}/>
-                    {/* <Route path='/contact' element={<Contact/>} /> */}
+                    <Route path='/contact' element={<Contact/>} />
+                    <Route path='/myproducts' element={<MyProducts/>} />
                     <Route path='/viewdetails' element={<ProductCard/>}/>
                 
                 </Routes>
                 <Contact/>
-                {/* <SlideShow /> */}
-                {/* < Home /> */}
-                    {/* <ProductCard/> */}
-                
-                    
-                {/* <AboutPage/> */}
             </BrowserRouter>
             {/* <Header/> */}
         </div>
