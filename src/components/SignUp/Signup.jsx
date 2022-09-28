23abab550568084a1dfe0c887145f5c4119a9c73
SohaibAlmomani <almomanisohaib95@yahoo.com>
2022-09-28 15:59:26 +0300

    some changes

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { signUp } from "../../auth/index";
 import { MDBBtn, MDBContainer, MDBCard, MDBCardBody, MDBCol, MDBRow, MDBInput, MDBCheckbox, MDBIcon } from "mdb-react-ui-kit";
+import LoginLogo from '../../Assests/logo.png';
 
 function Signup() {
   const [data, setData] = useState({
@@ -27,14 +28,13 @@ function Signup() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('00000000000000000');
+    console.log("00000000000000000");
     signUp(data).then((data) => {
       if (data.error) {
         setData({
           ...data,
           error: data.error,
           // success: false
-          
         });
       } else {
         setData({
@@ -82,23 +82,24 @@ function Signup() {
     // <MDBContainer fluid className='my-5'>
 
     <MDBContainer fluid>
-      <div className="p-5 bg-image" style={{ backgroundImage: "url(https://mdbootstrap.com/img/new/textures/full/171.jpg)", height: "300px" }}></div>
+      <div className="p-5 bg-image" style={{ backgroundColor: "#FEBD69", height: "300px" }}></div>
 
-      <MDBCard className="ml-22 mb-5 p-5 shadow-5 w-50" style={{ marginTop: "-100px", marginLeft: "22rem", background: "hsla(0, 0%, 100%, 0.8)", backdropFilter: "blur(30px)" }}>
-        <MDBCardBody className="p-5 text-center" >
-          <h2 className="fw-bold mb-5">Sign up now</h2>
+      <MDBCard className="ml-22 mb-5 p-5 shadow-5" style={{ width: "40rem", margin:'-16rem 0 0 27.5rem ' ,background: "hsla(0, 0%, 100%, 0.8)", backdropFilter: "blur(30px)" }}>
+        <MDBCardBody className="p-5 text-center">
+          {/* <h2 className="fw-bold mb-5">Sign up now</h2> */}
+          <img style={{width:'8rem'  , height:'8rem' }} src={LoginLogo}/>
 
           <MDBInput wrapperClass="mb-4" type="text" placeholder="Full Name" id="form1" value={username} onChange={handleChange("username")} />
           <MDBInput wrapperClass="mb-4" type="email" placeholder="example@gmail.com" id="form1" value={email} onChange={handleChange("email")} />
           <MDBInput wrapperClass="mb-4" type="password" placeholder="Password" id="form1" value={password} onChange={handleChange("password")} />
           <MDBInput wrapperClass="mb-4" type="text" placeholder="07********" value={phonenumber} onChange={handleChange("phonenumber")} id="form1" />
-          <MDBInput wrapperClass="mb-4" type="text" placeholder="Enter Your Address" value={adress} onChange={handleChange('adress')} id="form1" />
+          <MDBInput wrapperClass="mb-4" type="text" placeholder="Enter Your Address" value={adress} onChange={handleChange("adress")} id="form1" />
 
           {/* <div className="d-flex justify-content-center mb-4">
             <MDBCheckbox name="flexCheck" value="" id="flexCheckDefault" label="Subscribe to our newsletter" />
           </div> */}
 
-          <MDBBtn className="w-100 mb-4" color="dark" size="md" variant="primary" type="submit" onClick={handleSubmit} >
+          <MDBBtn style={{ backgroundColor: "#003566" }} className="w-100 mb-4" size="md" type="submit" onClick={handleSubmit}>
             sign up
           </MDBBtn>
 
